735481142e81dcb6057aa7cfe97ec123411bc705
Nick Karamoff <nikita.karamoff@gmail.com>
2017-11-12 15:59:34 +0300

    Add game screen (10)
     - add GameActivity
     - create link from button to the GameActivity using intent's Extra so
    that different buttons launch different game modes
     - add new gradient for Classic game mode
     - add icons for "Back" and "Pause" actions
     - other small tweaks

---
+++ app/src/main/java/ru/karamoff/kawan_kawan/arithmaster/GameActivity.java
@@ -0,0 +1,41 @@
+package ru.karamoff.kawan_kawan.arithmaster;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // this line hides the status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_game);
+
+        Intent intent = getIntent();
+        int gamemode = intent.getIntExtra("gamemode", 0); // получение информации о режиме игры
+
+        ConstraintLayout parent = findViewById(R.id.parentLayout);
+        TextView modeTitle = findViewById(R.id.modeTitle);
+
+        switch (gamemode) {
+            case 0:
+                parent.setBackground(getDrawable(R.drawable.classic_gradient));
+                modeTitle.setText(getText(R.string.classic_mode_name));
+                break;
+        }
+
+        ImageButton back = findViewById(R.id.backButton);
+        back.setOnClickListener(v -> {
+//            Intent intent = new Intent(GameActivity.this, MainActivity.class);
+//            startActivity(intent);
+            finish();
+        });
+    }
+
+}
