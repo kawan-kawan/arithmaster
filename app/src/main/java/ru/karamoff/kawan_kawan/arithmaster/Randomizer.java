3422a7c1c6c456e1ef37c4743fc156256f300c5a
Almaz <hamedzhanovalmaz@gmail.com>
2017-11-21 11:14:55 +0300

    Randomize numbers and operations (15)

--- app/src/main/java/ru/karamoff/kawan_kawan/arithmaster/Randomizer.java
+++ app/src/main/java/ru/karamoff/kawan_kawan/arithmaster/Randomizer.java
@@ -3,14 +3,20 @@ package ru.karamoff.kawan_kawan.arithmaster;
 /**
  * Created by nick on 14.11.17.
  */
+import java.util.Random;
 
 public class Randomizer {
+    private static int operation;
+    private static int number;
+    static Random random = new Random();
 
     static int generateOperation() {
-        return 0; //TODO прописать генерацию операций
+        operation = random.nextInt(3); //без деления
+        return operation;
     }
 
     static int generateNumber() {
-        return 0; //TODO прописать генерацию чисел
+        number = random.nextInt(50) + 1;
+        return number;
     }
 }
