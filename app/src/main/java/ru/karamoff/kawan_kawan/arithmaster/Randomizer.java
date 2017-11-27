2b8ef30f39e3746c3a1dda83d575fcf0ed2a5834
Nick Karamoff <nikita.karamoff@gmail.com>
2017-11-27 23:47:33 +0300

    Slight fix to Randomizer (23)
     - fix of Randomizer trying to access not yet initialized variables

--- app/src/main/java/ru/karamoff/kawan_kawan/arithmaster/Randomizer.java
+++ app/src/main/java/ru/karamoff/kawan_kawan/arithmaster/Randomizer.java
@@ -10,11 +10,7 @@ class Randomizer {
         int[] set = new int[4]; // 0 - операция, 1-2 - цифры, 3 - результат
 
         set[0] = random.nextInt(4);
-        if(set[0] == 3) { // топ развод проверки деления нацело
-            set[3] = set[1];
-            set[1] = set[3] * set[2];
-            return set;
-        }
+
         if(set[0] == 2) { // более маленькие числа для умножения
             set[1] = random.nextInt(20) + 1;
             if(set[1] < 6) {
@@ -22,13 +18,17 @@ class Randomizer {
             } else {
                 set[2] = random.nextInt(6) + 1;
             }
-           return set;
+        } else {
+            set[1] = random.nextInt(50) + 1;
+            set[2] = random.nextInt(50) + 1;
         }
 
-        set[1] = random.nextInt(50) + 1;
-        set[2] = random.nextInt(50) + 1;
-
         set[3] = Solver.solve(set[1], set[2], set[0]);
+
+        if(set[0] == 3) { // топ развод проверки деления нацело
+            set[3] = set[1];
+            set[1] = set[3] * set[2];
+        }
 
         return set;
     }
