578eb49e052427a41e0b7897154503e449ad7c6a
Nick Karamoff <nikita.karamoff@gmail.com>
2017-11-28 14:18:56 +0300

    Refactor and cleanup (27)
     - remove unused imports
     - improve access rules

--- app/src/main/java/ru/karamoff/kawan_kawan/arithmaster/Randomizer.java
+++ app/src/main/java/ru/karamoff/kawan_kawan/arithmaster/Randomizer.java
@@ -4,15 +4,15 @@ import java.util.Random;
 
 class Randomizer {
 
-    private static Random random = new Random();
+    private final static Random RANDOM = new Random();
 
 
     private static int[] setSmallNumber(int setS[]) {
-        setS[1] = random.nextInt(20) + 1;
+        setS[1] = RANDOM.nextInt(20) + 1;
         if (setS[1] < 6) {
-            setS[2] = random.nextInt(20) + 1;
+            setS[2] = RANDOM.nextInt(20) + 1;
         } else {
-            setS[2] = random.nextInt(6) + 1;
+            setS[2] = RANDOM.nextInt(6) + 1;
         }
         setS[3] = Solver.solve(setS[1], setS[2], setS[0]);
         return setS;
@@ -21,7 +21,7 @@ class Randomizer {
     static int[] generate() {
         int[] set = new int[4]; // 0 - операция, 1-2 - цифры, 3 - результат
 
-        set[0] = random.nextInt(4);
+        set[0] = RANDOM.nextInt(4);
 
         if (set[0] == 2) { // более маленькие числа для умножения
             set = setSmallNumber(set);
@@ -36,8 +36,8 @@ class Randomizer {
 
         }
 
-        set[1] = random.nextInt(50) + 1;
-        set[2] = random.nextInt(50) + 1;
+        set[1] = RANDOM.nextInt(50) + 1;
+        set[2] = RANDOM.nextInt(50) + 1;
         set[3] = Solver.solve(set[1], set[2], set[0]);
 
         return set;
