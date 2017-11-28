d9f4a4c1797ae6226383854b09dea635c0ea7c1b
Almaz <hamedzhanovalmaz@gmail.com>
2017-11-28 12:08:57 +0300

    randomize small numbers for / and *

--- app/src/main/java/ru/karamoff/kawan_kawan/arithmaster/Randomizer.java
+++ app/src/main/java/ru/karamoff/kawan_kawan/arithmaster/Randomizer.java
@@ -6,29 +6,38 @@ class Randomizer {
 
     private static Random random = new Random();
 
+
+    static void setSmallNumber(int setS[]) {
+        setS[1] = random.nextInt(20) + 1;
+        if (setS[1] < 6) {
+            setS[2] = random.nextInt(20) + 1;
+        } else {
+            setS[2] = random.nextInt(6) + 1;
+        }
+        setS[3] = Solver.solve(setS[1], setS[2], setS[0]);
+    }
+
     static int[] generate() {
-        int[] set = new int[4]; // 0 - операция, 1-2 - цифры, 3 - результат
+            int[] set = new int[4]; // 0 - операция, 1-2 - цифры, 3 - результат
 
         set[0] = random.nextInt(4);
 
         if(set[0] == 2) { // более маленькие числа для умножения
-            set[1] = random.nextInt(20) + 1;
-            if(set[1] < 6) {
-                set[2] = random.nextInt(20) + 1;
-            } else {
-                set[2] = random.nextInt(6) + 1;
-            }
+            setSmallNumber(set);
+            return set;
         } else {
-            set[1] = random.nextInt(50) + 1;
-            set[2] = random.nextInt(50) + 1;
+            if(set[0] == 3) { // топ развод проверки деления нацело
+                setSmallNumber(set);
+                set[3] = set[1];
+                set[1] = set[3] * set[2];
+                return set;
+            }
+
         }
 
+        set[1] = random.nextInt(50) + 1;
+        set[2] = random.nextInt(50) + 1;
         set[3] = Solver.solve(set[1], set[2], set[0]);
-
-        if(set[0] == 3) { // топ развод проверки деления нацело
-            set[3] = set[1];
-            set[1] = set[3] * set[2];
-        }
 
         return set;
     }
